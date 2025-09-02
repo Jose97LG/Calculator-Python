2a4f476ebd1fb41090dc7f8d855cb96fdc628b47
Jose <joseantoniolg97@gmail.com>
2025-09-02 21:12:02 +0200

    primeros pasos

---
+++ CodeCalcu.py
@@ -0,0 +1,30 @@
+Numero1 = float(input("Introduce el primer numero: "))
+Numero2 = float(input("Introduce el segundo numero: "))
+Operacion = input("¿Que operación quieres hacer? ")
+
+if Operacion == "sumar":
+    suma = Numero1 + Numero2
+print("La suma es : ", suma)
+
+if Operacion == "restar":
+    resta = Numero1 - Numero2
+print("La resta es : ", resultado)
+
+if Operacion == "multiplicar":
+    resultado = Numero1 * Numero2
+    resultado = Numero1 * Numero2
+print("La multiplicacion es : ", resultado)
+
+if Operacion == "dividir":
+    resultado = Numero1 / Numero2
+print("La division es : ", resultado)
+
+if Operacion == "fraccionar":
+    resultado = Numero1 / Numero2
+    resultado = Numero1 + Numero2
+print("La modulo es : ", resultado)
+
+if Operacion == "elevalo":
+    resultado = Numero1 ** Numero2
+print("La exponencial es : ", resultado)
+
